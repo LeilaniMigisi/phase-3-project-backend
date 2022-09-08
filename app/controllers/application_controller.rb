1f65e5260358edac07947363c0b8e56e1d032772
Leilani Migisi <leilani.migisi@student.moringaschool.com>
2022-09-09 00:42:48 +0300

    fixing controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,60 +1,33 @@
-# class ApplicationController < Sinatra::Base
-
-#   set :default_content_type, 'application/json'
-
-#     get '/reviews' do
-#       book= Book.all
-#         book.to_json(include: {reviews: {only: [:body], include: {user: {only: [:name]}}}})
-#       end
-
-#       post '/reviewss' do
-#         review = Review.create(
-#           body: params[:body],
-#           book_id: params[:book_id],
-#           user_id: params[:user_id]
-#         )
-#         review.to_json
-#       end
-
-#       get '/reviewss' do
-#         reviewss = Review.all
-#         reviewss.to_json
-
-#       end
-
-#       get '/users' do
-#         users = User.all
-#         users.to_json
-#       end
-
-#       # books routes
-
-#       get '/book' do 
-#         book = Book.all
-#         book.to_json
-#       end
-
-#       post '/book' do
-#         book = Book.create(
-#           name: params[:name],
-#           location: params[:location]
-#         )
-#         books.to_json
-#     end
+class ApplicationController < Sinatra::Base
+    set :default_content_type, 'application/json'
     
-#     delete '/book/:id' do 
-#       book = Books.find(params[:id])
-#       book.destroy
-#       book.to_json
+    get "/" do
+      { message: "Good reading!" }.to_json
+    end
+    
+    
+    get '/books/:id' do
+      airline = Book.find(params[:id])
   
-
-#     patch '/book/:id' do 
-#       book = Book.find(params[:id])
-#       book.update(
-#         name: params[:name],
-#         location: params[:location]
-#       )
-#       book.to_json
-#     end
-
-# end
+      
+      book.to_json(only: [:id, :name,  :image_url], include: {
+        reviews: { only: [:title, :description, :score], include: {
+          user: { only: [:name] }
+        } }
+      })
+      book.to_json
+    end
+  
+  
+  post  '/books/:id' do
+    book = Book.find(params[:id])
+   # include associated reviews in the JSON response
+    book.to_json(only: [:id, :name,:image_url],include: {
+      reviews: { only: [:title, :description, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+    book.to_json
+    
+  end
+  end 
